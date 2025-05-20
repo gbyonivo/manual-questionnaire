b65cb4ba3167763c8df886a6f1ca179e51fc5224
Orkuma Ivo <orkumaivo@Orkumas-MBP-2.broadband>
2025-05-20 13:56:39 +0100

    Refactor Body and Footer components for improved layout and styling; added social media icons and updated responsive design.

--- src/components/home/body.tsx
+++ src/components/home/body.tsx
@@ -3,11 +3,15 @@ import bodyDesc from "@/utils/body-desc.json";
 import { BodyLines } from "./body-lines";
 import { BodyImages } from "./body-images";
 
+// TODO: Show images on mobile and use reverse for second image
+
 export function Body() {
   const body = bodyDesc as unknown as BodyData;
   return (
-    <div className="pt-8 text-center">
-      <div className="text-4xl font-medium">{body.header}</div>
+    <div className="pt-8">
+      <div className="pl-4 text-2xl md:text-4xl font-medium text-left md:text-center">
+        {body.header}
+      </div>
       <div className="flex flex-col gap-2 md:gap-8">
         {body.parts.map((part) => {
           return (
--- src/components/home/footer.tsx
+++ src/components/home/footer.tsx
@@ -5,6 +5,16 @@ import {
   isFooterItemWithIcon,
   isFooterItemWithLink,
 } from "@/types/footer-data";
+// TODO: get icons from somewhere else
+import { Facebook, Instagram, Linkedin, Twitter, Youtube } from "lucide-react";
+
+const iconMap = {
+  facebook: <Facebook size={24} className="text-pink-800" />,
+  twitter: <Twitter size={24} className="text-pink-800" />,
+  instagram: <Instagram size={24} className="text-pink-800" />,
+  linkedin: <Linkedin size={24} className="text-pink-800" />,
+  youtube: <Youtube size={24} className="text-pink-800" />,
+};
 
 // TODO: Add color to tailwind config
 export function Footer() {
@@ -12,37 +22,49 @@ export function Footer() {
   const footerKeys = Object.keys(footerData);
 
   return (
-    <div className="p-32 flex flex-row" style={{ backgroundColor: "#e8efe9" }}>
-      <Logo className="mr-80 self-start" />
-      <div className="grid grid-cols-4 gap-4 flex-1">
-        {footerKeys.map((key) => {
-          const items = footerData[key];
-          return (
-            <div key={key} className="flex flex-col gap-2">
-              <h3>{key}</h3>
-              {items.map((item) => {
-                return (
-                  <div key={item.name}>
-                    {isFooterItemWithLink(item) && (
-                      <a href={item.link}>{item.name}</a>
-                    )}
-                    {isFooterItemWithIcon(item) && (
-                      <div className="flex flex-row gap-2">
-                        {item.icons.map((icon) => {
-                          return (
-                            <a href={icon.link} key={icon.icon}>
-                              {icon.icon}
-                            </a>
-                          );
-                        })}
-                      </div>
-                    )}
-                  </div>
-                );
-              })}
-            </div>
-          );
-        })}
+    <div
+      className="px-8 md:px-32 flex flex-row pt-16 mt-8"
+      style={{ backgroundColor: "#e8efe9" }}
+    >
+      <Logo className="hidden md:block md:mr-40 lg:mr-80 self-start" />
+      <div className="flex-1">
+        <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 ">
+          {footerKeys.map((key) => {
+            const items = footerData[key];
+            return (
+              <div key={key} className="flex flex-col gap-2">
+                <div className="font-bold uppercase text-xs">
+                  {key.replace(/_/g, " ")}
+                </div>
+                {items.map((item) => {
+                  return (
+                    <div key={item.name}>
+                      {isFooterItemWithLink(item) && (
+                        <a href={item.link} className="text-base">
+                          {item.name}
+                        </a>
+                      )}
+                      {isFooterItemWithIcon(item) && (
+                        <div className="flex flex-row gap-6">
+                          {item.icons.map((icon) => {
+                            return (
+                              <a href={icon.link} key={icon.icon}>
+                                {iconMap[icon.icon as keyof typeof iconMap]}
+                              </a>
+                            );
+                          })}
+                        </div>
+                      )}
+                    </div>
+                  );
+                })}
+              </div>
+            );
+          })}
+        </div>
+        <div className="w-full mt-20 border-t border-gray-300 text-center text-base py-8 text-gray-500">
+          © 2021 Manual. All rights reserverd
+        </div>
       </div>
     </div>
   );
